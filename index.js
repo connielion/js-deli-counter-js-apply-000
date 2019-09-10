2f28f15967bd7e3f47423692cbc88c65540c52ef
Connie Lai <connielion011@gmail.com>
2019-09-10 00:10:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 
 function takeANumber(line, new_name){
-  // find index of name in line, add 1
-  let position = line.indexOf(new_name) + 1;
-  // return string with template literal
   line.push(new_name);
+  const position = line.indexOf(new_name) + 1;
   return `Welcome, ${new_name}. You are number ${position} in line.`
 }
 
